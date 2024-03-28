bbde23cfc0e3576224ef5262b77366012a6ff24d
Shaggy-silverhand <felixbone54@yahoo.fr>
2024-03-28 20:55:06 +0100

    negroishimas

---
+++ Assets/People/Felix/Script/AudioManager.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------- Audio Source ---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------- Audio Clip ---------")]
+    public AudioClip background;
+    public AudioClip shoot;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
